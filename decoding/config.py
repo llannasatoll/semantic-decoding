fix bugs that come from wrong value of current_sec

decoding/config.py
@@ -3,9 +3,9 @@ import numpy as np
 import socket
 
 HOSTNAME = socket.gethostname() 
-gpu_device = "1"
-print("GPU DEVICE NO: ", gpu_device)
-os.environ['CUDA_VISIBLE_DEVICES'] = gpu_device
+# gpu_device = "0"
+# os.environ['CUDA_VISIBLE_DEVICES'] = gpu_device
+print("GPU DEVICE NO: ", os.environ['CUDA_VISIBLE_DEVICES'] )
 
 # paths
 
@@ -26,7 +26,7 @@ SCORE_DIR = os.path.join(REPO_DIR, "scores")
 
 TRIM = 5
 STIM_DELAYS = [1, 2, 3, 4]
-STIM_DELAYS = [2, 3, 4, 5]
+# STIM_DELAYS = [2, 3, 4, 5]
 RESP_DELAYS = [-4, -3, -2, -1]
 ALPHAS = np.logspace(4, 8, 17)
 NBOOTS = 15
@@ -62,6 +62,7 @@ MODELS = {
     'eng1000' : 'eng1000',
     'gpt2' : 'openai-community/gpt2',
     'llama3' : 'meta-llama/Meta-Llama-3-8B',
+    'llama3.1' : 'meta-llama/Meta-Llama-3.1-8B',
     'embed_small' : 'text-embedding-3-small',
     'embed_large' : 'text-embedding-3-large',
     'original' : os.path.join(os.path.join(REPO_DIR, "data_lm"), "perceived", "model"),

decoding/decode_words.py
@@ -47,50 +47,55 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--subject", type = str, required = True)
-    parser.add_argument("--llm", type = str, required = True)
     parser.add_argument("--em_id", type = str, required = True)
     parser.add_argument("--wr_id", type = str, required = True)
-    parser.add_argument("--num_chance", type = int, default = 100)
+    parser.add_argument("--generate_llm", type = str, required = True)
+    parser.add_argument("--em_llm", type = str, required = True)
+    parser.add_argument("--num_chance", type = int, required = True)
+    parser.add_argument("--only_null", action='store_true')
     args = parser.parse_args()
 
     test_stories = ['wheretheressmoke']
-    em_data = np.load(config.RESULT_DIR+'/%s/test/%s/encoding_model_%s.npz' % (args.subject, args.llm, args.em_id))
-    wr_data = np.load(config.RESULT_DIR+'/%s/test/%s/wordrate_model_%s.npz' % (args.subject, args.llm, args.wr_id))
-    assert em_data['model_path'] == wr_data['model_path']
+    em_data = np.load(config.RESULT_DIR+'/%s/test/%s/encoding_model_%s.npz' % (args.subject, args.em_llm, args.em_id))
+    wr_data = np.load(config.RESULT_DIR+'/%s/test/%s/wordrate_model_%s.npz' % (args.subject, args.generate_llm, args.wr_id))
+    logger.info(f"Word rate model: {wr_data['model_path']}")
+    logger.info(f"Encoding model: {em_data['model_path']}")
 
-    blank = " " if args.llm == "original" else ""
-    is_orig = (args.llm == "original")
+    blank = " " if args.generate_llm == "original" else ""
+    is_orig = (args.generate_llm == "original")
 
-    gpt = GPT(path = str(em_data['model_path']), device = config.GPT_DEVICE)
-    if args.llm != "original":
-        # gpt.model.generation_config.pad_token_id = gpt.tokenizer.pad_token_id  
-        gpt.tokenizer.pad_token_id = gpt.tokenizer.eos_token_id
-    features = LMFeatures(model = gpt, layer = em_data['layer'], context_words = -1)
+    generate_gpt = GPT(path = str(wr_data['model_path']), device = config.GPT_DEVICE)
+    if args.generate_llm != "original":
+        generate_gpt.model.generation_config.pad_token_id = generate_gpt.tokenizer.pad_token_id  
+        generate_gpt.tokenizer.pad_token_id = generate_gpt.tokenizer.eos_token_id
+    generate_features = LMFeatures(model = generate_gpt, layer = 1, context_words = -1)
 
     # Load and construct word rate model
     logger.info("Creating word rate model")
-    resp, rate, mean_wordrate = get_wr_feature_pair(args.subject, wr_data['train_stories'], features, str(wr_data['roi']))
-    resp_test, rate_test, _ = get_wr_feature_pair(args.subject, test_stories, features, str(wr_data['roi']))
+    resp, rate, mean_wordrate = get_wr_feature_pair(args.subject, wr_data['train_stories'], generate_features, str(wr_data['roi']))
+    resp_test, rate_test, _ = get_wr_feature_pair(args.subject, test_stories, generate_features, str(wr_data['roi']))
     clf = Ridge(alpha=wr_data['alpha'])
     clf.fit(resp, rate)
     word_rates = clf.predict(resp_test)
     word_rates = word_rates[:, 0] + mean_wordrate
     logger.info("mean corr=%0.5f" % (pearsonr(word_rates, rate_test[:, 0])[0]))
 
-    # Load and construct encoding  model
-    logger.info("Creating encoding model")
-    tmp = em_data['corr']
-    tmp[tmp < tmp[tmp.argsort()[-config.VOXELS]]] = 0
-    vox = [i for i in range(tmp.shape[0]) if tmp[i] != 0]
-    features.context_words = config.GPT_WORDS
-    wordvec, resp = get_em_feature_pair(args.subject, em_data['train_stories'], features, vox)
-    wordvec_test, resp_test = get_em_feature_pair(args.subject, test_stories, features, vox)
-    # clf = Ridge(alpha=em_data['alpha'][vox])
-    clf = Ridge(alpha=em_data['alpha'][vox].mean())
-    clf.fit(wordvec, resp)
-    assert resp_test.shape[0] == len(word_rates), f'{resp_test.shape[0]} != {len(word_rates)}'
-    del wordvec, resp
-    torch.cuda.empty_cache()
+    if not args.only_null:
+        # Load and construct encoding  model
+        logger.info("Creating encoding model")
+        em_gpt = GPT(path = str(em_data['model_path']), device = config.GPT_DEVICE)
+        em_features = LMFeatures(model = em_gpt, layer = em_data['layer'], context_words = config.GPT_WORDS)
+        tmp = em_data['corr']
+        tmp[tmp < tmp[tmp.argsort()[-config.VOXELS]]] = 0
+        vox = [i for i in range(tmp.shape[0]) if tmp[i] != 0]
+        wordvec, resp = get_em_feature_pair(args.subject, em_data['train_stories'], em_features, vox)
+        wordvec_test, resp_test = get_em_feature_pair(args.subject, test_stories, em_features, vox)
+        logger.warning("Using mean weight!!!!")
+        clf = Ridge(alpha=em_data['alpha'][vox].mean())
+        clf.fit(wordvec, resp)
+        assert resp_test.shape[0] == len(word_rates), f'{resp_test.shape[0]} != {len(word_rates)}'
+        del wordvec, resp
+        torch.cuda.empty_cache()
 
     fixed = 11 # sec
     unfixed_tr = int((17-fixed)/2) + 1
@@ -98,48 +103,52 @@ if __name__ == "__main__":
     word_rates = np.insert(word_rates, 0, [mean_wordrate for _ in range(unfixed_tr)])
     word_rates = np.array(list(map(round, word_rates)))
     if is_orig:
-        max_length = 512 - 50
+        max_length = 100
     else:
-        max_length = min(80, gpt.tokenizer.model_max_length - 50)
+        max_length = min(80, generate_gpt.tokenizer.model_max_length - 50)
 
     for story in test_stories:
         current_sec = fixed
         for i in range(len(word_seqs[story].data_times)):
             if word_seqs[story].data_times[i] > fixed: break
         if is_orig:
-            begin_words = word_seqs[story].data[:i]
-            begin_time = list(range(len(begin_words)))
+            raise()
+            # begin_words = word_seqs[story].data[:i]
+            # begin_time = list(range(len(begin_words)))
+            begin_words = []
+            begin_time = []
         else:
             begin_words, begin_time = get_beginning_info(str(em_data['model_path']), fixed)
         data_times = np.concatenate([word_seqs[story].data_times[:i][begin_time], np.linspace(fixed, 17, sum(word_rates[:unfixed_tr-1])+1)[:-1]])
 
         save_location = os.path.join(config.RESULT_DIR, args.subject, "decoding", "em%s_wr%s" % (args.em_id, args.wr_id))
-        if os.path.exists(save_location + "/%s_result.npz" % story):
-            data = np.load(save_location + "/%s_result.npz" % story)
+        if os.path.exists(save_location + "/%s_result_%d" % (story, args.num_chance)):
+            data = np.load(save_location + "/%s_result_%d" % (story, args.num_chance))
             start = len(data["chance_em"])-1
             logger.info(f"Restart at {start}.")
             if "Llama" in str(em_data['model_path']):
-                strs = [[[gpt.tokenizer.decode(ii) for ii in gpt.tokenizer.encode(s.decode()) if ii != gpt.tokenizer.bos_token_id] for s in data["can_stcs"][jj]] for jj in range(len(data["can_stcs"]))]
-                chance_strs = [[[gpt.tokenizer.decode(ii) for ii in gpt.tokenizer.encode(s.decode()) if ii != gpt.tokenizer.bos_token_id] for s in data["chance_stcs"][jj]] for jj in range(len(data["chance_stcs"]))]
+                strs = [[[em_gpt.tokenizer.decode(ii) for ii in em_gpt.tokenizer.encode(s.decode()) if ii != em_gpt.tokenizer.bos_token_id] for s in data["can_stcs"][jj]] for jj in range(len(data["can_stcs"]))]
+                chance_strs = [[[em_gpt.tokenizer.decode(ii) for ii in em_gpt.tokenizer.encode(s.decode()) if ii != em_gpt.tokenizer.bos_token_id] for s in data["chance_stcs"][jj]] for jj in range(len(data["chance_stcs"]))]
             elif "perceived" in str(em_data['model_path']): # which means original
                 strs = [[s.decode().split(" ") for s in data["can_stcs"][jj]] for jj in range(len(data["can_stcs"]))]
                 chance_strs = [[s.decode().split(" ") for s in data["chance_stcs"][jj]] for jj in range(len(data["chance_stcs"]))]
-                data_times = np.concatenate([data_times, ])
             else: 
                 strs = None
                 chance_strs = None
             for i in range(start):
                 current_sec += 2
-                data_times = np.concatenate([data_times, np.linspace(current_sec, current_sec+2, word_rates[i+unfixed_tr-1]+1)[:-1]])
+                # `current_sec` is right after generation time.
+                # If fixed is 11, the `current_sec` at the first generation step is 13`
+                data_times = np.concatenate([data_times, np.linspace(current_sec+4, current_sec+6, word_rates[i+unfixed_tr-1]+1)[:-1]])
             corr = data["can_corr"]
-            candidate = [[(wordlist, r) for wordlist, r in zip(strs[ii], corr[ii])] for ii in range(len(data["can_corr"]))]
+            candidate = [[(wordlist, r) for wordlist, r in zip(strs[ii], corr[ii])] for ii in range(len(data["can_corr"]))] if not args.only_null else []
             ref_strs = [s.decode().split(" ") for s in data["ref_stcs"]]
             ref_corr = data["ref_corr"]
             reference = [(wordlist, r) for wordlist, r in zip(ref_strs, ref_corr)]
             chance_corr = [data["chance_em"][:, ii] for ii in range(data["chance_em"].shape[-1])]
             chances = [[(wordlist, r) for wordlist, r in zip(chance_strs[ii], chance_corr[ii])] for ii in range(len(chance_corr))]
         else:
-            candidate = [[(begin_words, 1)]]
+            candidate = [[(begin_words, 1)]] if not args.only_null else []
             reference = [(word_seqs[story].data[:i], 1)]
             chances = [[(begin_words, 1)] for _ in range(args.num_chance)]
             start = 0
@@ -148,7 +157,7 @@ if __name__ == "__main__":
             current_sec += 2
             data_times = np.concatenate([
                 data_times, 
-                np.linspace(current_sec, current_sec+2, word_rates[i+unfixed_tr-1]+1)[:-1]
+                np.linspace(current_sec+4, current_sec+6, word_rates[i+unfixed_tr-1]+1)[:-1]
             ])
             num_words = sum(word_rates[i:i+unfixed_tr])
 
@@ -158,14 +167,14 @@ if __name__ == "__main__":
                 words = []
                 for ws, _ in words_corr_list:
                     words.extend(ws)
-                outputs = gpt.generate(blank.join(words[-max_length:]), num_words, config.WIDTH)
-                num_oldtok = len(words[-max_length:]) if is_orig else len(gpt.tokenizer.encode(blank.join(words[-max_length:])))
+                outputs = generate_gpt.generate(blank.join(words[-max_length:]), num_words, config.WIDTH)
+                num_oldtok = (len(words[-max_length:]) if len(words_corr_list) > 1 else 1) if is_orig else len(generate_gpt.tokenizer.encode(blank.join(words[-max_length:])))
                 for ii, sample_output in enumerate(outputs):
-                    tmp = [gpt.vocab[x] if x < len(gpt.vocab) else '<unk>' for x in sample_output[num_oldtok:]] \
-                        if is_orig else [gpt.tokenizer.decode(t, skip_special_tokens=True) for t in sample_output[num_oldtok:]]
+                    tmp = [generate_gpt.vocab[x] if x < len(generate_gpt.vocab) else '<unk>' for x in sample_output[num_oldtok:]] \
+                        if is_orig else [generate_gpt.tokenizer.decode(t, skip_special_tokens=True) for t in sample_output[num_oldtok:]]
                     if len(tmp) != num_words:
                         logger.warning(f'lack of num words: {len(tmp)} != {num_words}')
-                    del_mat = get_stim_from_wordslist(words+tmp, features, data_times, list(range(current_sec-(fixed-11), current_sec+(19-fixed)+1, 2)))
+                    del_mat = get_stim_from_wordslist(words+tmp, em_features, data_times, list(range(current_sec-(fixed-11), current_sec+(19-fixed)+1, 2)))
                     pred = clf.predict(del_mat[-1].reshape(1, -1))[0]
                     # `res[i]` is list of tuple of words list and correlation. len(res) is config.EXTENSIONS*config.WIDTH.
                     res.append(words_corr_list + [(tmp[:word_rates[i]], pearsonr(pred, resp_test[i])[0])])
@@ -174,29 +183,28 @@ if __name__ == "__main__":
                 words = []
                 for ws, _ in chance:
                     words.extend(ws)
-                outputs = gpt.generate(blank.join(words[-max_length:]), num_words, 1, do_sample=True)[0]
-                num_oldtok = len(words[-max_length:]) \
-                    if is_orig else len(gpt.tokenizer.encode(blank.join(words[-max_length:])))
-                tmp = [gpt.vocab[x] if x < len(gpt.vocab) else '<unk>' for x in outputs[num_oldtok:]] \
-                    if is_orig else [gpt.tokenizer.decode(t) for t in outputs[num_oldtok:]]
+                outputs = generate_gpt.generate(blank.join(words[-max_length:]), num_words, 1, do_sample=True)[0]
+                num_oldtok = (len(words[-max_length:]) if len(words_corr_list) > 1 else 1) if is_orig else len(generate_gpt.tokenizer.encode(blank.join(words[-max_length:])))
+                tmp = [generate_gpt.vocab[x] if x < len(generate_gpt.vocab) else '<unk>' for x in outputs[num_oldtok:]] \
+                    if is_orig else [generate_gpt.tokenizer.decode(t) for t in outputs[num_oldtok:]]
                 if len(tmp) != num_words:
                     logger.warning(f'lack of num words (chance): {len(tmp)} != {num_words}')
                     logger.warning(tmp)
-                del_mat = get_stim_from_wordslist(words+tmp, features, data_times, list(range(current_sec-(fixed-11), current_sec+(19-fixed)+1, 2)))
+                del_mat = get_stim_from_wordslist(words+tmp, em_features, data_times, list(range(current_sec-(fixed-11), current_sec+(19-fixed)+1, 2)))
                 pred = clf.predict(del_mat[-1].reshape(1, -1))[0]
                 chance.append((tmp[:word_rates[i]], pearsonr(pred, resp_test[i])[0]))
 
             candidate = sorted(res, key=lambda x: x[-1][1])[::-1][:config.EXTENSIONS] # sort by pearson where is [1]. [::-1] means DECENT.
             ref_corr = pearsonr(clf.predict(wordvec_test[i].reshape(1, -1))[0], resp_test[i])[0]
             reference.append((
-                list(np.array(word_seqs[story].data)[(word_seqs[story].data_times < current_sec) & (word_seqs[story].data_times >= current_sec-(17-fixed))]),
+                list(np.array(word_seqs[story].data)[(word_seqs[story].data_times < current_sec) & (word_seqs[story].data_times >= current_sec-2)]),
                 ref_corr
                 ))
             logger.info([can[-1] for can in candidate])
             logger.info(reference[-1])
 
             os.makedirs(save_location, exist_ok = True)
-            np.savez(os.path.join(save_location, "%s_result" % story),
+            np.savez(os.path.join(save_location, "%s_result_%d" % (story, args.num_chance)),
                 can_stcs=np.array([list(map(lambda x: blank.join(x[0]).encode(), candidate[i])) for i in range(config.EXTENSIONS)]),
                 can_corr=np.array([list(map(lambda x: x[1], candidate[i])) for i in range(config.EXTENSIONS)]),
                 ref_stcs=np.array(list(map(lambda x: ' '.join(x[0]).encode(), reference))),

decoding/decode_words_with_2model.py
@@ -82,8 +82,8 @@ if __name__ == "__main__":
     # Load and construct encoding  model
     logger.info("Creating encoding model")
     em_gpt = GPT(path = str(em_data['model_path']), device = config.GPT_DEVICE)
-    em_features = LMFeatures(model = em_gpt, layer = 11, context_words = config.GPT_WORDS)
-    # em_features = LMFeatures(model = em_gpt, layer = em_data['layer'], context_words = config.GPT_WORDS)
+    # em_features = LMFeatures(model = em_gpt, layer = 11, context_words = config.GPT_WORDS)
+    em_features = LMFeatures(model = em_gpt, layer = em_data['layer'], context_words = config.GPT_WORDS)
     tmp = em_data['corr']
     tmp[tmp < tmp[tmp.argsort()[-config.VOXELS]]] = 0
     vox = [i for i in range(tmp.shape[0]) if tmp[i] != 0]
@@ -128,11 +128,11 @@ if __name__ == "__main__":
             if "perceived" in str(wr_data['model_path']): # which means original
                 strs = [[s.decode().split(" ") for s in data["can_stcs"][jj]] for jj in range(len(data["can_stcs"]))]
                 chance_strs = [[s.decode().split(" ") for s in data["chance_stcs"][jj]] for jj in range(len(data["chance_stcs"]))]
-                data_times = np.concatenate([data_times, ])
+                # data_times = np.concatenate([data_times, ])
             else: 
                 strs = None
                 chance_strs = None
-            for i in range(start+1):
+            for i in range(start):
                 current_sec += 2
                 data_times = np.concatenate([data_times, np.linspace(current_sec, current_sec+2, word_rates[i+unfixed_tr-1]+1)[:-1]])
             corr = data["can_corr"]

decoding/eval_bertscore.old.py
@@ -1,11 +1,10 @@
 import os
 import sys
-import pickle
 import logging
-import subprocess
 import argparse
 import numpy as np
 from tqdm import tqdm
+from bert_score import BERTScorer
 
 import config
 
@@ -19,39 +18,51 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--subject", type = str, required = True)
-    parser.add_argument("--decode_res_id", type = str, required = True)
+    parser.add_argument("--id", type = str, required = True)
     parser.add_argument("--candidate", type = int, default = 0)
     args = parser.parse_args()
 
     test_stories = ['wheretheressmoke']
+    tmp_path = os.path.join('/home', 'anna', 'semantic-decoding', 'tmp', 'tmp%d.txt')
     for story in test_stories:
-        with open(os.path.join(config.RESULT_DIR, args.subject, 'decoding', f'{args.decode_res_id}.pickle'), 'rb') as f:
-            candidate = list(map(lambda x: x[0], pickle.load(f)[args.candidate]))
-        with open(os.path.join(config.RESULT_DIR, args.subject, 'decoding', 'reference', story, f'{args.decode_res_id}.pickle'), 'rb') as f:
-            reference = list(map(lambda x: x[0], pickle.load(f)))
+        result = np.load(os.path.join(config.RESULT_DIR, args.subject, "decoding", args.id, "%s_result.npz" % story))
+        bertscores = {'f1': [], 'chance': [[] for _ in range(result["chance_em"].shape[-1])]}
+        chances = result['chance_stcs']
 
-        assert len(candidate) == len(reference)
-
-        res = []
-        for i in tqdm(range(len(candidate))):
-            can_tmp = ''.join(candidate[max(0,i-5):i+5]).replace('\n', '')
-            ref_tmp = ' '.join(reference[max(0,i-5):i+5]).replace('\n', '')
-
-            with open('tmp1.txt', 'w') as f:
+        for i in tqdm(range(len(result['ref_corr'])-1)):
+            can_tmp = ''.join([stc.decode() for stc in result['can_stcs'][args.candidate][1:][max(0,i-5):i+5]]).replace('\n', '').replace('<|begin_of_text|>', '')
+            ref_tmp = ' '.join([stc.decode() for stc in result['ref_stcs'][1:][max(0,i-5):i+5]]).replace('\n', '')
+            with open(tmp_path%1, 'w') as f:
                 f.write(can_tmp+'\n')
-            with open('tmp2.txt', 'w') as f:
+            with open(tmp_path%2, 'w') as f:
                 f.write(ref_tmp+'\n')
-            cmd = ['bert-score', '--lang', 'en', '--rescale_with_baseline', '--model', 'microsoft/deberta-xlarge-mnli', \
-            '--num_layers', '40', '-r', 'tmp1.txt', '-c', 'tmp2.txt']
-            result = subprocess.run(cmd, capture_output=True, text=True)
-            tmp = result.stdout.split('rescaled_fast-tokenizer ')[-1].replace('P: ','').replace('R: ','').replace('F1: ','').split()
-            assert len(tmp) == 3
-            res.append(tmp)
 
-        save_location = os.path.join(config.RESULT_DIR, args.subject, "decoding", "bertscore")
+            idf_sents = np.load(os.path.join(config.DATA_TEST_DIR, "idf_segments.npy"))
+            metric = BERTScorer(lang = "en", rescale_with_baseline = False, idf = (idf_sents is not None), idf_sents = idf_sents)
+            score_id = 1
+            f1_tmp = metric.score(cands = [can_tmp], refs = [ref_tmp])[score_id].numpy()[0]
+
+            bertscores['f1'].append(f1_tmp)
+            logger.info(ref_tmp)
+            logger.info(f"[CAND] {f1_tmp}: {can_tmp}")
+                
+            for j in range(len(chances)):
+                can_tmp = ''.join(list(map(lambda x: x.decode(), chances[j][1:][max(0,i-5):i+5]))).replace('\n', '')
+                with open(tmp_path%1, 'w') as f:
+                    f.write(can_tmp+'\n')
+                stdout = ''
+                f1_tmp = metric.score(cands = [can_tmp], refs = [ref_tmp])[score_id].numpy()[0]
+                try:
+                    if stdout != '': f1_tmp = float(stdout.split('F1: ')[-1])
+                    logger.info(f"{f1_tmp}, {can_tmp}")
+                    bertscores['chance'][j].append(f1_tmp)
+                except:
+                    if stdout != '': logger.warning(f"WARNING: Could not extract the score from {stdout}, {can_tmp}")
+                    bertscores['chance'][j].append(0)
+
+        save_location = os.path.join(config.RESULT_DIR, args.subject, "decoding", args.id)
         os.makedirs(save_location, exist_ok = True)
-        np.savez(os.path.join(save_location, args.decode_res_id),
-            precision = list(map(lambda x:float(x[0]), res)), \
-            recall = list(map(lambda x:float(x[1]), res)), \
-            f1 = list(map(lambda x:float(x[2]), res))
-            )
+        np.savez(os.path.join(save_location, "%s_bertscore_original" % story),
+            f1 = np.array(bertscores['f1']),
+            chance_f1 = np.array(bertscores['chance'])
+        )

decoding/eval_bertscore.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 from bert_score import BERTScorer
 
 import config
+from utils_stim import get_story_wordseqs
 
 if __name__ == "__main__":
 
@@ -22,37 +23,57 @@ if __name__ == "__main__":
     parser.add_argument("--llm", type = str, required = True)
     parser.add_argument("--large", action='store_true')
     parser.add_argument("--candidate", type = int, default = 0)
+    parser.add_argument("--num_chance", type = int, default = 0)
     args = parser.parse_args()
 
     test_stories = ['wheretheressmoke']
     tmp_path = os.path.join('/home', 'anna', 'semantic-decoding', 'tmp', 'tmp%d.txt')
 
+    idf_sents = np.load(os.path.join(config.DATA_TEST_DIR, "idf_segments.npy"))
+    kwargs = {
+        "lang": "en",
+        "rescale_with_baseline": False,
+        "idf": (idf_sents is not None),
+        "idf_sents": idf_sents
+    }
+    if args.large:
+        kwargs["model_type"] = "microsoft/deberta-xlarge-mnli"
+    metric = BERTScorer(
+        lang = "en",
+        rescale_with_baseline = False, 
+        idf = (idf_sents is not None),
+        idf_sents = idf_sents, 
+        # model_type = "microsoft/deberta-v2-xxlarge",
+        # num_layers=44
+    )
+    score_id = 1
     for story in test_stories:
-        result = np.load(os.path.join(config.RESULT_DIR, args.subject, "decoding", args.id, "%s_result.npz" % story).replace("/home", "/Storage2"))
-        bertscores = {'f1': [], 'chance': [[] for _ in range(result["chance_em"].shape[-1])]}
+        save_location = os.path.join(config.RESULT_DIR, args.subject, "decoding", args.id).replace("/home", "/Storage2")
+        path = os.path.join(save_location, "%s_result%s" % (story,("" if args.num_chance < 0 else "_" + str(args.num_chance))))
+        logger.info(path)
+        result = np.load(path + ".npz")
+        if args.num_chance:
+            bertscores = {'f1': [], 'chance': [[] for _ in range(result["chance_em"].shape[-1])]}
+        else:
+            bertscores = {'f1': [], 'chance': [[]]}
         chances = result['chance_stcs']
 
+        word_seqs = get_story_wordseqs([story])
         blank = " " if args.llm == "original" else ""
+        fixed = 11
+        current_sec = fixed
         try:
+            # for i in tqdm(range(10)):
             for i in tqdm(range(len(result['ref_corr'])-1)):
+                current_sec += 2
                 can_tmp = blank.join([stc.decode() for stc in result['can_stcs'][args.candidate][1:][max(0,i-5):i+5]]).replace('\n', '').replace('<|begin_of_text|>', '')
-                ref_tmp = ' '.join([stc.decode() for stc in result['ref_stcs'][1:][max(0,i-5):i+5]]).replace('\n', '')
+                ref_tmp = ' '.join(list(np.array(word_seqs[story].data)[(word_seqs[story].data_times < current_sec+10) & (word_seqs[story].data_times >= max(fixed, current_sec-10))]))
+                # ref_tmp = ' '.join([stc.decode() for stc in result['ref_stcs'][1:][max(0,i-5):i+5]]).replace('\n', '')
                 with open(tmp_path%1, 'w') as f:
                     f.write(can_tmp+'\n')
                 with open(tmp_path%2, 'w') as f:
                     f.write(ref_tmp+'\n')
 
-                idf_sents = np.load(os.path.join(config.DATA_TEST_DIR, "idf_segments.npy"))
-                kwargs = {
-                    "lang": "en",
-                    "rescale_with_baseline": False,
-                    "idf": idf_sents is not None,
-                    "idf_sents": idf_sents
-                }
-                if args.large:
-                    kwargs["model_type"] = "microsoft/deberta-xlarge-mnli"
-                metric = BERTScorer(**kwargs)
-                score_id = 1
                 f1_tmp = metric.score(cands = [can_tmp], refs = [ref_tmp])[score_id].numpy()[0]
 
                 bertscores['f1'].append(f1_tmp)
@@ -60,27 +81,22 @@ if __name__ == "__main__":
                 logger.info(f"[CAND] {f1_tmp}: {can_tmp}")
 
                 for j in range(len(chances)):
-                    can_tmp = ''.join(list(map(lambda x: x.decode(), chances[j][1:][max(0,i-5):i+5]))).replace('\n', '')
+                    can_tmp = blank.join(list(map(lambda x: x.decode(), chances[j][1:][max(0,i-5):i+5]))).replace('\n', '')
                     with open(tmp_path%1, 'w') as f:
                         f.write(can_tmp+'\n')
                     f1_tmp = metric.score(cands = [can_tmp], refs = [ref_tmp])[score_id].numpy()[0]
-                    try:
-                        logger.info(f"{f1_tmp}, {can_tmp}")
-                        bertscores['chance'][j].append(f1_tmp)
-                    except:
-                        bertscores['chance'][j].append(0)
+                    logger.info(f"{f1_tmp}, {can_tmp}")
+                    bertscores['chance'][j].append(f1_tmp)
 
-            save_location = os.path.join(config.RESULT_DIR, args.subject, "decoding", args.id).replace("/home", "/Storage2")
             os.makedirs(save_location, exist_ok = True)
-            np.savez(os.path.join(save_location, "%s_bertscore" % story),
+            np.savez(path.replace("_result", "_bertscore") + ("_large" if args.large else ""),
                 f1 = np.array(bertscores['f1']),
                 chance_f1 = np.array(bertscores['chance']),
             )
         except Exception as e:
             logger.warning(f"Exception occurred : {e}. Saving at {i}")
-            save_location = os.path.join(config.RESULT_DIR, args.subject, "decoding", args.id)
             os.makedirs(save_location, exist_ok = True)
-            np.savez(os.path.join(save_location, "%s_bertscore" % story),
+            np.savez(path.replace("_result", "_bertscore") + ("_large" if args.large else ""),
                 f1 = np.array(bertscores['f1']),
                 chance_f1 = np.array(bertscores['chance']),
             )